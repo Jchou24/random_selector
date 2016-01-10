c9edd7b9bd3e620240816eb6d236b09f3e022fcb
Jchou24 <johnny1991524@gmail.com>
2016-01-10 09:05:33 +0800

    update

--- rs.py
+++ rs.py
@@ -89,7 +89,7 @@ def age3y():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
 
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
